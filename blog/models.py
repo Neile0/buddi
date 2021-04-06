70d11fb6e54cb35bc10f964794a24e2150ed3600
Li-Jen Chen <2582307c@student.gla.ac.uk>
2021-04-06 01:50:08 +0100

    model for blog Li

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime
+NAME_MAX_LENGTH = 128
+
 
 # Create your models here.
+class shop(models.Model):
+    topic = models.CharField(max_length=NAME_MAX_LENGTH)
+    contend = models.FileField(upload_to='contend')
+    image = models.FileField(upload_to='image')
+
+class vet(models.Model):
+    topic = models.CharField(max_length=NAME_MAX_LENGTH)
+    contend = models.FileField(upload_to='contend')
+    image = models.FileField(upload_to='image')
+
+class news(models.Model):
+    topic = models.CharField(max_length=NAME_MAX_LENGTH)
+    contend = models.FileField(upload_to='contend')
+    image = models.FileField(upload_to='image')
+    date = models.DateTimeField(default=datetime.now(), editable=False)
