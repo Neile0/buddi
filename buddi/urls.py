587cab9d7c5909e3bb063a9ba2733e9db37cbd48
Ioana Roca <2434068R@student.gla.ac.uk>
2021-04-08 00:35:05 +0300

    User and sitter templates and views

--- buddi/urls.py
+++ buddi/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('user/<username>/', views.user_profile, name="user"),
+    path('sitter/<username>/', views.sitter_profile, name="sitter"),
     path('become-a-buddi/', views.register, name='register'),
     path('find-sitter/', views.find_sitter, name='find-sitter'),
     ]
