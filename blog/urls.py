f0eeeb20cb2b9d9065c6353b63b4050574440aac
Li-Jen Chen <2582307c@student.gla.ac.uk>
2021-04-07 11:59:15 +0100

    blog template

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path("<str:topic>", views.news_detail, name='news')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,24 @@
+from django.core.files import File
+from django.core.files.images import ImageFile
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 
 def index(request):
     return render(request, 'blog/index.html')
+
+
+def news_detail(request, topic):
+    detail = news.objects.get(topic=topic)
+
+    read = detail.contend.readlines()
+
+    print(detail.image.url)
+    contextbox = {
+        "topic" : detail.topic,
+        "contend" : read,
+        "pic" : detail.image.url
+    }
+    return render(request, "blog/news-detail.html", context=contextbox)
