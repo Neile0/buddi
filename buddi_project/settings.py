2dfa9ff95335f5b3c425934558bc225ec82c882d
Aidan Neil <29099867+Neile0@users.noreply.github.com>
2021-04-07 19:12:14 +0100

    Adding a session timeout

--- buddi_project/settings.py
+++ buddi_project/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -134,3 +135,11 @@ MEDIA_URL = '/media/'
 # Login
 
 LOGIN_URL = 'buddi:login'
+
+# Session
+
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+
+SESSION_EXPIRE_SECONDS = 1800  # 1 hour
+
+SESSION_TIMEOUT_REDIRECT = 'buddi:login'
