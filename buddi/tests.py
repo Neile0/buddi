856c21360c6cb2e00f91d6951dacc682d4dbf235
Li-Jen Chen <2582307c@student.gla.ac.uk>
2021-04-09 09:12:23 +0100

    test 70%

--- buddi/tests.py
+++ buddi/tests.py
@@ -6,6 +6,7 @@ from blog.models import *
 from .views import *
 
 
+
 # Create your tests here.
 
 class blogReportConfigTest(TestCase):
@@ -153,8 +154,70 @@ class populateBlog_test(TestCase):
         self.check_conntend(News_contend_list, Vet_contend_list, Shop_contend_list)
 
 
-class testBuddiView(TestCase):
-    pass
+class testIndexView(TestCase):
+    def setUp(self):
+        self.response = self.client.get(reverse('buddi:index'))
+
+    def test_index_use_template(self):
+        self.assertTemplateUsed(self.response, 'buddi/index.html',
+                               f"Your index() view does not use the expected index.html template.")
+
+    def test_index_uses_context_dictionary(self):
+
+        self.assertTrue('regions' in self.response.context_dict,
+                        f"You didn't pass any information in this page")
+
+        message = self.response.context['regions']
+        expected = get_parent_regions()
+        self.assertEqual(message, expected,
+                         f"You did't parse correct information")
+
+
+class testUserProfileView(TestCase):
+    def setUp(self):
+
+        self.response = self.client.get(reverse('buddi:user_profile'))
+
+    def test_UserProfile_use_template(self):
+        self.assertTemplateUsed(self.response, 'buddi/user_profile.html',
+                               f"Your user_profile() view does not use the expected user_profile.html template.")
+
+
+
+
+
+class testSitter(TestCase):
+    def setUp(self):
+        self.response = self.client.get(reverse('buddi:sitter'))
+
+    def test_Sitter_use_template(self):
+        self.assertTemplateUsed(self.response, 'buddi/sitter_profile.html',
+                               f"Your user_profile() view does not use the expected sitter_profile.html template.")
+
+
+
+class testSitterProfile(TestCase):
+    def setUp(self):
+        self.response = self.client.get(reverse('buddi:sitter_profile'))
+
+    def test_Sitter_Profile_use_template(self):
+        self.assertTemplateUsed(self.response, 'buddi/sitter.html',
+                               f"Your user_profile() view does not use the expected sitter.html template.")
+
+
+class testSit(TestCase):
+    def setUp(self):
+        self.response = self.client.get(reverse('buddi:sit'))
+
+    def test_sit_use_template(self):
+        self.assertTemplateUsed(self.response, 'buddi/search.html',
+                               f"Your user_profile() view does not use the expected search.html template.")
+
+
+
+
+
+
 
 
 
